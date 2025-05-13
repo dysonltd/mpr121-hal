cec89df577aa62cc08c1d66138db6870f4853813
Scott <scott.gibb@dyson.com>
2025-05-13 11:22:39 +0100

    improved docs

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-/// Threshold values for the touch and release threshold
+//! Constant values for various parts of the drive
 
 pub const DEFAULT_TOUCH_THRESHOLD: u8 = 12;
 pub const DEFAULT_RELEASE_THRESOLD: u8 = 6;
